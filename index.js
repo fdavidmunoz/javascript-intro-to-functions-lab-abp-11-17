b6e98dcc1e2f2e203bb3716ac86817aa89bac983
Francisco David Muñoz <fdavidmunoz@gmail.com>
2017-11-12 18:46:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   } else if (string.toUpperCase() === string) {
-      return "YES INDEED"
+      return "YES INDEED!"
   }
 }
